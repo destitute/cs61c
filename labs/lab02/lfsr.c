796d96f673e3bfca055691c58fe3f6ccf5d1b203
destitute <needy.mya@gmail.com>
2022-03-05 19:34:44 +0800

    lab02 done

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -5,6 +5,10 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+  /* YOUR CODE HERE */
+  uint16_t ans = *reg;
+  int sign = (ans & 1) ^ ((ans >> 2) & 1) ^ ((ans >> 3) & 1) ^ ((ans >> 5) & 1);
+  ans >>= 1;
+  ans |= (sign << 15);
+  *reg = ans;
 }
-
