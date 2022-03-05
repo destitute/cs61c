4a9a98fd2ab7e02256160bb52ee819972c272632
destitute <needy.mya@gmail.com>
2022-03-05 15:49:18 +0800

    lab01 done

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -2,6 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+  /* your code here */
+  if (head == NULL)
     return 0;
+
+  node *fast = head, *slow = head;
+  while (fast->next && fast->next->next) {
+    fast = fast->next->next;
+    slow = slow->next;
+
+    if (fast == slow)
+      return 1;
+  }
+  return 0;
 }
